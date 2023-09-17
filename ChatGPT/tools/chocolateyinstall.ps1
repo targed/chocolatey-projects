db0b08cbbe437736555ea4b83e41db78af14cf3a
kaiserl4961 <ltklionel@gmail.com>
2023-09-17 15:34:48 -0500

    updated chatgpt to 1.1.0

--- ChatGPT/tools/chocolateyinstall.ps1
+++ ChatGPT/tools/chocolateyinstall.ps1
@@ -6,9 +6,9 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'msi'
-  url = 'https://github.com/lencx/ChatGPT/releases/download/v0.12.0/ChatGPT_0.12.0_windows_x86_64.msi'
+  url            = 'https://github.com/lencx/ChatGPT/releases/download/v1.1.0/ChatGPT_1.1.0_windows_x86_64.msi'
   softwareName   = 'ChatGPT*'
-  checksum       = 'ed09b660a16215844f5072e6ea4821c17dcaa3eb9e3a48e864d9428850d48973'
+  checksum       = '8442A2ED4D74230A08A3AAB94B658C6250C37197519D86BE7F334C57F8B76625'
   checksumType   = 'sha256'
   
   silentArgs     = '/quiet'
