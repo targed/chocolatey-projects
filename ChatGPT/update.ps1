2dcc8bc72c7a3d233636a95ebbd4d6831040cbc7
asheroto <github@asheroto.com>
2023-09-22 00:37:08 -0500

    Adjust ChatGPT nuspec file

--- ChatGPT/update.ps1
+++ ChatGPT/update.ps1
@@ -3,20 +3,20 @@ import-module au
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
 
 
 function global:au_GetLatest {
-    $domain   = 'https://github.com'
+    $domain = 'https://github.com'
     $releases = "$domain/lencx/ChatGPT/releases/"
     $pattern = "/v(\d+\.\d+\.\d+)/"
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing #1
-    $re    = '\.msi$'
-    $url   = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
+    $re = '\.msi$'
+    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
     $url = $domain + $url
 
     if ($url -match $pattern) {
