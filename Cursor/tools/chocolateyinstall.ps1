c1968a69cdf41b0cd914334d71bad25f54d05976
kaiserl4961 <ltklionel@gmail.com>
2024-04-20 14:52:12 -0500

    Got the cursor package working

--- Cursor/tools/chocolateyinstall.ps1
+++ Cursor/tools/chocolateyinstall.ps1
@@ -11,7 +11,7 @@ $packageArgs = @{
   checksum       = '8006853A651B907D6FF8BA396D2119C9F85403D82DAC872B48294BCC44D64DDD'
   checksumType   = 'sha256'
   
-  silentArgs     = "/SD /v /qn"
+  silentArgs     = '/S'
   validExitCodes = @(0, 3010, 1641)
 }
 
