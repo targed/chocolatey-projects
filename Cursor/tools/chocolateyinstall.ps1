893060e38a4a7c7612e330ddb48286b1dd8c31ec
publicfacingusername <github@mail4j.com>
2024-10-13 23:29:59 -0400

    Upgrade cursor from 0.41.3 to 0.42.2

--- Cursor/tools/chocolateyinstall.ps1
+++ Cursor/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType       = 'exe'
   url            = 'https://downloader.cursor.sh/windows/nsis/x64'
   softwareName   = 'Cursor*'
-  checksum       = '2786F80344085CA6657C9F283F97B98C61A933E27CCB7D34B682CC3EA5F9A9FE'
+  checksum       = 'A66A0C5306E13BFB1479AE4456D0AED72B9C956182F9CDAD47CE21884C81E65E'
   checksumType   = 'sha256'
   
   silentArgs     = '/S'
