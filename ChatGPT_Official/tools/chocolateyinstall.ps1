3646f7af7d9abf3b10957b0e9428712c1735adc8
kaiserl4961 <ltklionel@gmail.com>
2024-10-23 21:43:29 -0500

    Got the official chatgpt to build

--- ChatGPT_Official/tools/chocolateyinstall.ps1
+++ ChatGPT_Official/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  fileType       = 'msi'
-  url            = 'https://github.com/lencx/ChatGPT/releases/download/v1.1.0/ChatGPT_1.1.0_windows_x86_64.msi'
-  softwareName   = 'ChatGPT*'
-  checksum       = '8442A2ED4D74230A08A3AAB94B658C6250C37197519D86BE7F334C57F8B76625'
+  fileType       = 'exe'
+  url            = 'https://get.microsoft.com/installer/download/9NT1R1C2HH7J?hl=en-us'
+  softwareName   = 'ChatGPT-Desktop*'
+  checksum       = '829B69A35A8AE782F4D3F0288B077A8F8DEC804C6B97EE48AE2F44E90F5883F9'
   checksumType   = 'sha256'
   
   silentArgs     = '/quiet'
