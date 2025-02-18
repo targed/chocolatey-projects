5022cc0d37e091a8a8c9ddfc6acd3c3e53dc2ec6
targed <ltklionel@gmail.com>
2025-02-17 22:00:36 -0600

    fixed jan

--- Jan/tools/chocolateyinstall.ps1
+++ Jan/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $VerbosePreference = 'SilentlyContinue'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version = "0.5.14"
-$url = "https://github.com/janhq/jan/releases/download/jan-win-x64-${version}.exe"
+$url = "https://github.com/janhq/jan/releases/download/v${version}/jan-win-x64-${version}.exe"
 $checksum = "65A90BBA5BDDA7B5EAEE212C50873CBA82168833B2C185BE4832C94FA45C3BE0"
 
 $packageArgs = @{
@@ -14,11 +14,8 @@ $packageArgs = @{
   url            = $url
   checksum       = $checksum
   checksumType   = "sha256"
-  silentArgs     = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /NOICONS /NOCANCEL /NOLOG"
+  silentArgs     = "/S /VERYSILENT /SUPPRESSMSGBOXES /norestart /quiet /qn /norestart /l*v /SP- $locale"
   validExitCodes = @(0, 3010, 1641)
 }
 
 Install-ChocolateyPackage @packageArgs
-
-# Install a shortcut on the desktop
-# Install-ChocolateyShortcut -shortcutFilePath "$env:USERPROFILE\Desktop\TumblThree.lnk" -targetPath "$($packageArgs.unzipLocation)\TumblThree.exe"
