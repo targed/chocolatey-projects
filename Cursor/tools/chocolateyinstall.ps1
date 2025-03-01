fe21cb75ed97cca170591bebd4c8717c3e7d153f
Joao C Costa <joaocc@users.noreply.github.com>
2025-03-01 16:10:17 +0000

    Upgrading and fixing wrong HASH from prev version

--- Cursor/tools/chocolateyinstall.ps1
+++ Cursor/tools/chocolateyinstall.ps1
@@ -6,9 +6,9 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  url            = 'https://anysphere-binaries.s3.us-east-1.amazonaws.com/production/3611c5390c448b242ab97e328493bb8ef7241e61/win32/x64/user-setup/CursorUserSetup-x64-0.46.7.exe'
+  url            = 'https://anysphere-binaries.s3.us-east-1.amazonaws.com/production/be4f0962469499f009005e66867c8402202ff0b7/win32/x64/user-setup/CursorUserSetup-x64-0.46.8.exe'
   softwareName   = 'Cursor*'
-  checksum       = '81DAB6FE2C887A5BB8C0245F8AB2C26D898711397FC30442B656CC9971E20334'
+  checksum       = 'D89F7AE12592ABC3600221FCAF4B6AB127EF4DB9AB721875F3DE07EB35A52527'
   checksumType   = 'sha256'
   
   silentArgs     = '/SILENT'
