07da2b0b8c29fd1c91b5b6c69870c190555196f6
kaiserl4961 <ltklionel@gmail.com>
2025-05-12 01:04:25 -0500

    Make cursor autoupdate

--- Cursor/tools/chocolateyinstall.ps1
+++ Cursor/tools/chocolateyinstall.ps1
@@ -6,9 +6,9 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'exe'
-  url            = 'https://anysphere-binaries.s3.us-east-1.amazonaws.com/production/ae378be9dc2f5f1a6a1a220c6e25f9f03c8d4e19/win32/x64/user-setup/CursorUserSetup-x64-0.46.11.exe'
+  url            = 'https://downloads.cursor.com/production/0781e811de386a0c5bcb07ceb259df8ff8246a52/win32/x64/user-setup/CursorUserSetup-x64-0.49.6.exe'
   softwareName   = 'Cursor*'
-  checksum       = '56BE08FF0CBFBA41CE86BD78B43420ABEC6232AA2ADCBD5ABF98210A9DF3E1C0'
+  checksum       = '78A5BC17FFAF9C8B382AD07D0D97237D1F32A0350061A5DF8D8EE903BBD7D674'
   checksumType   = 'sha256'
   
   silentArgs     = '/VERYSILENT'
