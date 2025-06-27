197042a0fd5b05114179eba710263c1400b9bf00
kaiserl4961 <ltklionel@gmail.com>
2025-06-27 15:25:44 -0500

    Update chocolateyinstall.ps1

--- Jan/tools/chocolateyinstall.ps1
+++ Jan/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $VerbosePreference = 'SilentlyContinue'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version = "0.5.17"
-$url = "https://github.com/janhq/jan/releases/download/v${version}/jan-win-x64-${version}.exe"
+$url = "https://github.com/menloresearch/jan/releases/download/v{VERSION}/Jan_{VERSION}_x64-setup.exe"
 $checksum = "E7B3B0432E873399110F0605C1995C843EC7B6856A5131D26DB34B4A6048CD68"
 
 $packageArgs = @{
