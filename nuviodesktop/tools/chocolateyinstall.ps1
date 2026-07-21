a2e424ca7c6924f5f86ec9743485c04cecd73b59
targed <ltklionel@gmail.com>
2026-07-20 22:43:34 -0500

    updated nuvio

--- nuviodesktop/tools/chocolateyinstall.ps1
+++ nuviodesktop/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
   fileType       = 'MSI'
-  url            = 'https://github.com/NuvioMedia/NuvioDesktop/releases/download/0.1.12-alpha/Nuvio-Windows-x64-0.1.12-alpha.msi'
+  url            = 'https://github.com/NuvioMedia/NuvioDesktop/releases/download/0.1.13-alpha/Nuvio-Windows-x64-0.1.13-alpha.msi'
   softwareName   = 'nuviodesktop*'
   checksum       = '820BF39613B67632796A15436A03412E1B2CA607D8C113BA0ECCDCDA8DF23ABF'
   checksumType   = 'sha256'
-  silentArgs     = '/quiet /qn /norestart'
+  silentArgs     = '/quiet /qn /norestart\'
   validExitCodes = @(0, 3010, 1641)
 }
 
