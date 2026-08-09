0ac3c866f8a05854592b33671709a29c24b7136d
targed <ltklionel@gmail.com>
2026-08-09 14:46:02 -0500

    Updated reasonix-cli install

--- reasonix-cli/tools/chocolateyinstall.ps1
+++ reasonix-cli/tools/chocolateyinstall.ps1
@@ -7,10 +7,10 @@ $packageArgs = @{
   fileType       = 'ZIP'
   url            = 'https://github.com/esengine/DeepSeek-Reasonix/releases/download/v1.21.5/reasonix-windows-amd64.zip'
   softwareName   = 'reasonix-cli*'
-  checksum       = '20B2D9B03A25C6DB5788F24815428BC6C48C458CD4509D6D3D05FB481FB66DA9'
+  checksum       = '3C34350798772F408C0D8ADEE15800E4EB6B9A6D8A5324D75D04B72B47BCCF81'
   checksumType   = 'sha256'
   silentArgs     = '/S /VERYSILENT /SUPPRESSMSGBOXES /norestart /quiet /qn /norestart /l*v /SP- $locale'
   validExitCodes = @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
